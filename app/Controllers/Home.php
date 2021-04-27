f5a625584ff9cad2fc807d40de96076ab125fb45
patriciajosianne <patriciajosianne@gmail.com>
2021-04-27 07:33:43 -0300

    Controller, view, menu

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -2,10 +2,45 @@
 
 namespace App\Controllers;
 
+/**
+ * Classe responsável por carregar a estrutura do app
+ */
 class Home extends BaseController
 {
-	public function index()
+	
+	public function index(): void 
 	{
-		return view('welcome_message');
+		$data['title'] = "Home";
+		$data['conteudo'] = view('home/index');
+
+		$this->modelo($data);
+	}
+
+	public function contatos(): void
+	{
+		$data['title'] = "Contatos";
+		$data['conteudo'] = view('home/contatos');
+
+		$this->modelo($data);
+	}
+
+	public function contato(): void
+	{
+		$data['title'] = "Contato";
+		$data['conteudo'] = view('home/contato');
+
+		$this->modelo($data);
+	}
+
+	private function modelo($data): void 
+	{
+		$menu['menus'] = [
+			'Home' => base_url(),
+			'Contatos' => base_url('contatos'),
+			'Contato' => base_url('contato')
+		];		
+		$data['menu'] = view('home/padrao/menu', $menu, ['cache' => 60]);
+
+		echo view('home/padrao/modelo', $data);
 	}
 }
