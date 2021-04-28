454fe7f57da99871ebe0cd460b5ce8ff1371eccb
patriciajosianne <patriciajosianne@gmail.com>
2021-04-28 10:50:45 -0300

    - Criação de arquivo de migração. - Criação do model TipoContatoModel. - Criação do Seeder para TipoContato.

---
+++ app/Models/TipoContatoModel.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use CodeIgniter\Model;
+
+class TipoContatoModel extends Model
+{
+	protected $DBGroup              = 'default';
+	protected $table                = 'tipos_contato';
+	protected $primaryKey           = 'id';
+	protected $returnType           = 'array';
+	protected $useSoftDelete        = true;
+	protected $protectFields        = true;
+	protected $allowedFields        = ['nome'];
+
+	// Dates
+	protected $useTimestamps        = true;
+	protected $dateFormat           = 'datetime';
+	protected $createdField         = 'criado_em';
+	protected $updatedField         = 'atualizado_em';
+	protected $deletedField         = 'apagado_em';
+
+	// Validation
+	protected $validationRules      = [
+		'nome' => 'required|min_length[3]|max_length[45]',
+	];
+	protected $validationMessages   = [];
+
+}
